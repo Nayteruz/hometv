8e98adc0db49138bef63e7d89c69dfc440db6703
Aleksandr Pankov <nayteruz@gmail.com>
2024-12-01 20:15:44 +0700

    add button icon

--- src/components/HeaderFilm.vue
+++ src/components/HeaderFilm.vue
@@ -20,7 +20,11 @@
 			<button type="submit">Найти</button>
 		</form>
 		<RegistrationWrap />
-		<button type="button" @click="toggleSearch" class="search-popup-btn"></button>
+		<button
+			type="button"
+			@click="toggleSearch"
+			:class="{ 'search-popup-btn': true, 'opened': isSearchVisible }"
+		></button>
 	</header>
 </template>
 
@@ -124,12 +128,17 @@ a.favorites {
 .search-popup-btn {
 	border: none;
 	border-radius: 5px;
-	background: url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' height='1em' viewBox='0 0 512 512'%3E%3Cpath d='M416 208c0 45.9-14.9 88.3-40 122.7L502.6 457.4c12.5 12.5 12.5 32.8 0 45.3s-32.8 12.5-45.3 0L330.7 376c-34.4 25.2-76.8 40-122.7 40C93.1 416 0 322.9 0 208S93.1 0 208 0S416 93.1 416 208zM208 352a144 144 0 1 0 0-288 144 144 0 1 0 0 288z'/%3E%3C/svg%3E")
+	background: url('data:image/svg+xml;utf8,<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path fill="%23032746" d="M416 208c0 45.9-14.9 88.3-40 122.7L502.6 457.4c12.5 12.5 12.5 32.8 0 45.3s-32.8 12.5-45.3 0L330.7 376c-34.4 25.2-76.8 40-122.7 40C93.1 416 0 322.9 0 208S93.1 0 208 0S416 93.1 416 208zM184 296c0 13.3 10.7 24 24 24s24-10.7 24-24l0-64 64 0c13.3 0 24-10.7 24-24s-10.7-24-24-24l-64 0 0-64c0-13.3-10.7-24-24-24s-24 10.7-24 24l0 64-64 0c-13.3 0-24 10.7-24 24s10.7 24 24 24l64 0 0 64z"/></svg>')
 		50% 50% no-repeat #80b0d9;
 	font-size: 0;
 	line-height: 0;
 	width: 35px;
 	height: 100%;
+	background-size: 60%;
+
+	&.opened {
+		background-image: url('data:image/svg+xml;utf8,<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path fill="%23032746" d="M416 208c0 45.9-14.9 88.3-40 122.7L502.6 457.4c12.5 12.5 12.5 32.8 0 45.3s-32.8 12.5-45.3 0L330.7 376c-34.4 25.2-76.8 40-122.7 40C93.1 416 0 322.9 0 208S93.1 0 208 0S416 93.1 416 208zM136 184c-13.3 0-24 10.7-24 24s10.7 24 24 24l144 0c13.3 0 24-10.7 24-24s-10.7-24-24-24l-144 0z"/></svg>');
+	}
 }
 
 form {
