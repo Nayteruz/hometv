09cc2a5dc45eb471509ac90261d7b6333added24
Alexander Pankov <nayteruz@gmail.com>
2026-02-05 23:16:04 +0700

    fix manifest 4

--- vite.config.ts
+++ vite.config.ts
@@ -8,7 +8,13 @@ export default defineConfig({
     vue(),
     VitePWA({
       registerType: 'autoUpdate',
-      includeAssets: ['favicon.ico', 'apple-touch-icon.png'],
+      injectRegister: 'auto',
+      includeAssets: [
+        'favicon.ico',
+        'apple-touch-icon.png',
+        'img/icons/android-chrome-192x192.png',
+        'img/icons/android-chrome-512x512.png',
+      ],
       manifest: {
         name: 'Фильмы и сериалы',
         short_name: 'Фильмы и сериалы',
@@ -39,10 +45,29 @@ export default defineConfig({
       },
       workbox: {
         globPatterns: ['**/*.{js,css,html,ico,png,svg,json,vue,txt,woff2}'],
-        navigateFallback: '/hometv/index.html', // Важно для SPA
+        navigateFallback: '/hometv/index.html',
+        // Убедитесь, что SW корректно кэширует все
+        runtimeCaching: [
+          {
+            urlPattern: /^https:\/\/nayteruz\.github\.io\/hometv\/.*/i,
+            handler: 'NetworkFirst',
+            options: {
+              cacheName: 'hometv-cache',
+              expiration: {
+                maxEntries: 50,
+                maxAgeSeconds: 30 * 24 * 60 * 60, // 30 дней
+              },
+            },
+          },
+        ],
       },
+      strategies: 'generateSW',
+      srcDir: 'src',
+      filename: 'service-worker.js',
       devOptions: {
-        enabled: true, // Включает PWA в режиме разработки для тестирования
+        enabled: true,
+        type: 'module',
+        navigateFallback: '/hometv/index.html',
       },
     }),
   ],
