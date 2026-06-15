a23797e37e8aac262344b38dc9aff8d56458251a
Alexander Pankov <nayteruz@gmail.com>
2026-06-15 23:03:13 +0700

    Move keys to github secrets

--- src/plugins/firebaseConfig.ts
+++ src/plugins/firebaseConfig.ts
@@ -1,8 +1,8 @@
 export default {
-  apiKey: 'AIzaSyDapqMH_oM7h6CDKM0laMGrLIlDVDyb_gY',
-  authDomain: 'hometv-c10f8.firebaseapp.com',
-  projectId: 'hometv-c10f8',
-  storageBucket: 'hometv-c10f8.appspot.com',
-  messagingSenderId: '452296723769',
-  appId: '1:452296723769:web:982a4288b9cf4e189bace8',
+  apiKey: import.meta.env.VITE_API_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_API_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_API_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_API_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_API_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_API_FIREBASE_APP_ID,
 };
